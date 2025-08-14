d518b1e453b7f96c53261133b0026db0c3ebdee1
laynn <2210817120012@mhs.ulm.ac.id>
2025-08-14 15:58:35 +0800

    add atb tabel pma

---
+++ database/migrations/2025_08_14_075742_create_pma_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('pma', function (Blueprint $table) {
+            $table->id(); // PK
+            $table->string('kabupaten_kota'); // atau bisa FK kalau ada tabel kabupaten_kota
+            $table->year('tahun');
+            $table->string('periode', 20);
+            $table->integer('proyek_pma');
+            $table->bigInteger('tambahan_investasi_dalam_ribu_usd');
+            $table->bigInteger('tambahan_investasi_dalam_juta');
+            
+            // Relasi ke Data_Investasi
+            $table->unsignedBigInteger('id_data');
+            $table->foreign('id_data')->references('id_data')->on('data_investasi')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pma');
+    }
+};
