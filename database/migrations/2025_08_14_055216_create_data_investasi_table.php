f7a5609be6b008217d6c95e383ffe28deec8823b
nanutt <najahmaisya03@gmail.com>
2025-08-15 11:45:19 +0800

    update tabel data investasi

--- database/migrations/2025_08_14_055216_create_data_investasi_table.php
+++ database/migrations/2025_08_14_055216_create_data_investasi_table.php
@@ -13,37 +13,27 @@ class CreateDataInvestasiTable extends Migration
     {
         Schema::create('data_investasi', function (Blueprint $table) {
             $table->id('id_data');
-            $table->year('tahun');
-            $table->string('periode', 50);
+            $table->integer('tahun');
+            $table->string('periode', 40);
             $table->string('status_penanaman_modal', 10);
-            $table->string('regional', 100)->nullable();
-            $table->string('negara', 100);        
-            $table->string('sektor_utama', 100)->nullable();
-            $table->string('nama_sektor', 150);       
+            $table->string('regional', 100);
+            $table->unsignedBigInteger('negara')->nullable();
+            $table->unsignedBigInteger('sektor_utama')->nullable();
+            $table->string('nama_sektor', 150);
             $table->string('deskripsi_kbli_2digit', 255);
             $table->string('provinsi', 100);
-            $table->string('kabupaten_kota', 100);  
+            $table->unsignedBigInteger('kabupaten_kota')->nullable();
             $table->string('wilayah_jawa', 40);
             $table->string('pulau', 50);
-            $table->decimal('investasi_rp_juta', 15, 2)->nullable();
-            $table->decimal('investasi_us_ribu', 15, 2)->nullable();
+            $table->bigInteger('investasi_rp_juta')->nullable();
+            $table->bigInteger('investasi_us_ribu')->nullable();
             $table->integer('jumlah_tki')->nullable();
-            
-
-            $table->foreign('negara')
-                  ->references('negara')->on('negara_investor')
-                  ->onDelete('cascade');
-
-            $table->foreign('nama_sektor')
-                  ->references('nama_sektor')->on('sektor')
-                  ->onDelete('cascade');
-
-            $table->foreign('kabupaten_kota')
-                  ->references('kabupaten_kota')->on('lokasi')
-                  ->onDelete('cascade');
-            
-            $table->index(['tahun', 'periode']);
             $table->timestamps();
+
+            // Contoh jika nanti ingin relasi foreign key
+            // $table->foreign('negara')->references('id')->on('negara');
+            // $table->foreign('sektor_utama')->references('id')->on('sektor_utama');
+            // $table->foreign('kabupaten_kota')->references('id')->on('kabupaten_kota');
         });
     }
 
