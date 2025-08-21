73f92cbc555b74bbba7f23d06559d236b9a85a66
nanutt <najahmaisya03@gmail.com>
2025-08-21 11:09:05 +0800

    update tampilan index

--- resources/views/admin/data_investasi/index.blade.php
+++ resources/views/admin/data_investasi/index.blade.php
@@ -12,20 +12,27 @@
             <div class="col-sm-4 mb-3">
                 <div class="card h-100">
                     <div class="card-header">
-                        <h5 class="card-title">Data Investasi Tahun {{ $data->tahun }}</h5>
+                        <h3 class="card-title">Data Realisasi Investasi </h3>
                     </div>
                     <div class="card-body">
+                        <p><strong>ID:</strong> {{ $data->id }}</p>
+                        <p><strong>Tahun:</strong>{{ $data->tahun}}</p>                         
                         <p><strong>Periode:</strong> {{ $data->periode }}</p>
-                        <p><strong>Status Modal:</strong> {{ $data->status_penanaman_modal ?? '-' }}</p>
+                        <p><strong>Status Penanaman Modal:</strong> {{ $data->status_penanaman_modal ?? '-' }}</p>
+                        <p><strong>Regional:</strong> {{ $data->regional ?? '-' }}</p>
                         <p><strong>Negara:</strong> {{ $data->negara ?? '-' }}</p>
+                        <p><strong>Sektor Utama:</strong> {{ $data->sektor_utama ?? '-' }}</p>
                         <p><strong>Nama Sektor:</strong> {{ $data->nama_sektor ?? '-' }}</p>
+                        <p><strong>Deskripsi KBLI 2 Digit:</strong> {{ $data->deskripsi_kbli_2digit ?? '-' }}</p>
                         <p><strong>Provinsi:</strong> {{ $data->provinsi ?? '-' }}</p>
                         <p><strong>Kab/Kota:</strong> {{ $data->kabupaten_kota ?? '-' }}</p>
+                        <p><strong>Jawa / Luar Jawa:</strong> {{ $data->wilayah_jawa ?? '-' }}</p>
+                        <p><strong>Pulau:</strong> {{ $data->pulau ?? '-' }}</p>
                         <p><strong>Investasi Rp:</strong> 
-                           {{ isset($data->investasi_rp_juta) ? number_format($data->investasi_rp_juta, 0, ',', '.') : '-' }}
+                           {{ isset($data->investasi_rp_juta) ? number_format($data->investasi_rp_juta, 2, ',', '.') : '-' }}
                         </p>
                         <p><strong>Investasi US$:</strong> 
-                           {{ isset($data->investasi_us_ribu) ? number_format($data->investasi_us_ribu, 0, ',', '.') : '-' }}
+                           {{ isset($data->investasi_us_ribu) ? number_format($data->investasi_us_ribu, 2, ',', '.') : '-' }}
                         </p>
                         <p><strong>Jumlah TKI:</strong> {{ $data->jumlah_tki ?? '-' }}</p>
                     </div>
