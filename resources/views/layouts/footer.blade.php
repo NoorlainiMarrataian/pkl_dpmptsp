178f09559c955c1028508f18c0810ca5a8a0c576
melisaanggini <melisaanggini92@gmail.com>
2025-08-22 10:35:59 +0800

    menambahkan header dan footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+    <div class="footer-container">
+        <!-- Hubungi Kami -->
+        <div class="footer-col">
+            <h4>Hubungi Kami</h4>
+            <address>
+                Jalan Bangun Praja, Kawasan Perkantoran Pemerintah Provinsi Kalimantan Selatan,<br>
+                Cempaka, Banjarbaru, Kalimantan Selatan<br>
+                📞 0511 - 6749344 | 📱 0811 506 1000<br>
+                📧 set@dpmptsp.kalselprov.go.id
+            </address>
+        </div>
+
+        <!-- Jam Kerja -->
+        <div class="footer-col">
+            <h4>Jam Kerja Layanan</h4>
+            <p>Senin – Kamis: 08.00 – 15.00 WITA</p>
+            <p>Jum’at: 08.00 – 11.00 & 13.00 – 15.00 WITA</p>
+        </div>
+
+        <!-- Ikuti Kami -->
+        <div class="footer-col">
+            <h4>Ikuti Kami</h4>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+                <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                <a href="#"><i class="fab fa-youtube"></i> YouTube</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        © 2025 DPMPTSP Provinsi Kalimantan Selatan. All rights reserved.
+    </div>
+</footer>
+
+{{-- FontAwesome --}}
+<script src="https://kit.fontawesome.com/yourkitid.js" crossorigin="anonymous"></script>
